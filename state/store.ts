8c301b0a80c7941d0979cb6a2bbe5fe4d194de0b
bbsmithy <briansmith.work578@gmail.com>
2023-03-22 15:58:00 +0000

    logger middleware added

--- state/store.ts
+++ state/store.ts
@@ -3,12 +3,20 @@ import OnboardingReducer from './reducers/OnboardingReducer'
 import ActionsReducer from './reducers/ActionsReducer'
 import TodayReducer from './reducers/TodayReducer'
 
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
 const store = configureStore({
   reducer: {
     OnboardingReducer,
     ActionsReducer,
     TodayReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
